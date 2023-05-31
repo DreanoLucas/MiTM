e169cec5998362a93b1412fa2df5582e533a0587
MANGLE DJIRO Chadi <12209067@crit-xubuntu>
2023-05-31 13:10:52 +0200

    ouais

---
+++ script.sh
@@ -0,0 +1,13 @@
+if [ $1 = "clone" ]
+	then
+	git clone "https://github.com/DreanoLucas/SAE24.git"
+fi
+if [ $1 = "push" ]
+	then 
+	git add ~/SAE24/ 
+	git commit
+	echo -n "Quelle branche ?"
+	read $branch
+	git push origin $branch
+	echo ouais
+fi
