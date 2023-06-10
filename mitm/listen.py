e7dcc8595f47ac3a853945760d21c8273d94c027
Chadi Manglé <chadimangled@gmail.com>
2023-06-10 15:29:53 +0200

    Creation de la fonction http

--- mitm/listen.py
+++ mitm/listen.py
@@ -1,14 +1,32 @@
-# def http(ip,nb):
-    
+'''Fichier contenant les fonctions permetant de lire les paquettes envoyés dans un réseau'''
+import scapy 
 from scapy.all import sniff
 from scapy.layers.http import HTTPRequest
+from datetime import datetime
 
-def check(p):
-    if HTTPRequest in p:  # le paquet contient une requÃªte HTTP
-        req = p[HTTPRequest]
-        print(
-            req.Method.decode("utf-8"),
-            req.Path.decode("utf-8"),
-            req.Http_Version.decode("utf-8")
+import sys
+
+def check(paquet:scapy)->None:
+    """Procedure affichant les paquets HTTP \n
+    paquet: tous les paquets transitant dans le réseau
+    """
+    if HTTPRequest in paquet:  #Si le paquet contient une requète HTTP
+        req = paquet[HTTPRequest]
+
+        print("{};{};{};{}".format(
+                            datetime.now(),
+                            paquet[0][1].dst,
+                            req.Method.decode("utf-8"),
+                            req.Path.decode("utf-8"))
         )
-sniff(prn=check)
+
+# sniff(prn=check)
+
+def http(ip, nb=60):
+    sniff(prn=check, 
+          count=nb, 
+          filter='src host {}'.format(ip),
+          timeout=nb,
+          iface="enp0s3")
+    
+http(sys.argv[1])
