4402fd58292c8e9bd8c3cb6b088a8cb0bdbbe3bc
Chadi Manglé <chadimangled@gmail.com>
2023-06-10 16:32:41 +0200

    listen.py j'avance mais c'est pas fini je sais pas si c'est timeout qui faut faire et voila quoi

--- mitm/listen.py
+++ mitm/listen.py
@@ -13,20 +13,24 @@ def check(paquet:scapy)->None:
     if HTTPRequest in paquet:  #Si le paquet contient une requète HTTP
         req = paquet[HTTPRequest]
 
-        print("{};{};{};{}".format(
+        print("{} ; {} ; {} ; {}".format(
                             datetime.now(),
                             paquet[0][1].dst,
                             req.Method.decode("utf-8"),
                             req.Path.decode("utf-8"))
         )
 
-# sniff(prn=check)
-
-def http(ip, nb=60):
+def http(ip, nb=120):
+    """Affiche les trames http capturées d'une ip spécifique. \n
+    ip: IP ciblée 
+    nb: Nombre de seconde de capture des trames
+    """
+    print(f"Lecture des trames http de {ip} durant {nb}s.")
     sniff(prn=check, 
           count=nb, 
           filter='src host {}'.format(ip),
           timeout=nb,
           iface="enp0s3")
+    print("Fin de transmission")
     
 http(sys.argv[1])
