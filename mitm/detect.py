bed9fbfddd2c00f61b5b9c166c93e0b78f963596
Chadi-Manglé <chadimangled@gmail.com>
2023-06-11 12:10:24 +0200

    arp table warking

--- mitm/detect.py
+++ mitm/detect.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python3
+import scapy
 from scapy.all import Ether, ARP, sniff, sendp, srp
 
-ip_to_mac = {}
-
-def arp_watch(paquet)->None:
+def arp_watch(paquet:scapy)->None:
+    global ip_to_mac
     if ARP in paquet and paquet[ARP].op in (1,2):
+        print(ip_to_mac)
         mac = paquet[ARP].hwsrc
         ip = paquet[ARP].psrc
         try: 
             if mac != ip_to_mac[ip]:
-                print("Empoisonnement ARP detecté pour cette IP : {}".format(ip))
-                p  = Ether() / ARP(op=2, hwsrc=ip_to_mac[ip] ,psrc=ip)
-                sendp(p)
-                print("Empoisonnement ARP déjoué !")
+                print(f"""Empoisonnement ARP detecté pour cette IP : {ip}\nAncienne adresse MAC : {ip_to_mac}\nNouvelle adresse MAC : {mac} (celle du pirate)""")
+                # p  = Ether() / ARP(op=2, hwsrc=ip_to_mac[ip] ,psrc=ip)
+                # sendp(p)
+                # print("Empoisonnement ARP déjoué !")
         except:  
             ip_to_mac[ip] = mac
 
-
-def arp_table(target_ip):
+def arp_table(target_ip:str)->dict:
     packet =Ether(dst="ff:ff:ff:ff:ff:ff") /  ARP(pdst=target_ip)
     result = srp(packet, timeout=3, verbose=0)[0]
 
@@ -29,9 +29,9 @@ def arp_table(target_ip):
     return arp_table
 
 def arp():
-    ip_to_mac = arp_table("192.168.1.0/24")
-    print(ip_to_mac)
     sniff(filter='arp', prn=arp_watch)
+
+ip_to_mac = arp_table("192.168.1.0/24")
 
 if __name__ == "__main__":
     arp()
