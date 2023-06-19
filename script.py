7cd40c42679eebd60998e55c948ceb4e86d2ebd4
Dreano Lucas <49568908+DreanoLucas@users.noreply.github.com>
2023-06-19 17:11:55 +0200

    Rajout de docstring et optimisation

---
+++ script.py
@@ -0,0 +1,23 @@
+from sys import argv 
+from mitm import atk, listen, detect
+'''Script permetant de lancer facilement les fonction principale du paquet mitm \n 
+Les arguments utilisables sont:
+arp, http, dns, detect_arp. ''' 
+
+try: 
+    if  argv[1] == "arp":
+        ipa = input("adresse1 : ")
+        ipb = input("adresse2 : ")
+        atk.arp(ipa, ipb)   
+    elif argv[1] == "http":
+        ip = input("adresse : ")
+        listen.http(ip)
+    elif argv[1] == "dns":
+        ip = input("adresse : ")
+        listen.dns(ip)
+    elif argv[1] == "detect_arp":
+        detect.arp()
+    raise ValueError
+except: 
+    print("Les fonctions à utiliser sont les suivantes:")
+    help(script)
