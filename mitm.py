f87790220470e4bdcb0526d17a71ce5697f61295
Dreano Lucas <49568908+DreanoLucas@users.noreply.github.com>
2023-06-19 16:56:20 +0200

    Ajout de l'ecoute DNS

--- mitm.py
+++ mitm.py
@@ -1,7 +1,7 @@
 import sys
 from mitm import atk, listen, detect
 
-if sys.argv[1] == "arp1":
+if sys.argv[1] == "arp":
     ipa = input("adresse1 : ")
     ipb = input("adresse2 : ")
     
@@ -9,7 +9,10 @@ if sys.argv[1] == "arp1":
 elif sys.argv[1] == "http":
     ip = input("adresse : ")
     listen.http(ip)
-elif sys.argv[1] == "arp2":
+elif sys.argv[1] == "dns":
+    ip = input("adresse : ")
+    listen.dns(ip)
+elif sys.argv[1] == "detect_arp":
     detect.arp()
 else: 
     print("Ouais force utilise une option valide.")
